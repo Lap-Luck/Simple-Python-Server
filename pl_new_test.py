4b6419e9b2c68c8ad68b229a1537d7289b0917b3
Lap-Luck <111698032+Lap-Luck@users.noreply.github.com>
2023-04-29 15:23:00 +0200

    remove debug print,add can clam draw

--- pl_new_test.py
+++ pl_new_test.py
@@ -10,6 +10,7 @@ import json
 #time.sleep(delay)
 game_id=str(100)#str(random.randrange(1,1234))
 names= ['alg1','alg2']
+full_print=False
 
 def ai(chess_message):
     board=chess_message["board"]
@@ -17,21 +18,21 @@ def ai(chess_message):
     halfmove_clock=chess_message["halfmove_clock"]
     last_move=chess_message["last_move"]
     print(board)
-    print(legal_moves)
+    if full_print: print(legal_moves)
     return legal_moves[-1]
 
 def play(player_id,sever_talker):
-    print("Pl" + str(id) + " hello")
+    if full_print: print("Pl" + str(id) + " hello")
     res=sever_talker('{"type":"connect","id":"' + str(player_id) + '"}')
     print(player_id,">",res)
 
     playing=True
     move_num=0
-    print("Enter game",player_id)
+    if full_print:print("Enter game",player_id)
     while playing:
         while True:
             can_move=sever_talker('{"type":"can_move","id":"' + str(player_id) + '"}')=='True'
-            print("can_move",can_move)
+            if full_print: print("can_move",can_move)
             if not can_move:
                 sever_talker('{"type":"wait","id":"' + str(player_id) + '"}')
             else:
@@ -51,6 +52,8 @@ def play(player_id,sever_talker):
                 print("$$$$$ERROR$$$$$$$$")
                 json_message=sever_talker('{"type":""}')
 
+        if bool(chess_message['can_claim_draw']):
+            print("claim_draw")
 
         if len(chess_message['outcome'])>6:
             print(chess_message['outcome'])
@@ -60,7 +63,7 @@ def play(player_id,sever_talker):
         move:str=ai(chess_message)
         print("MOVE",player_id,move)
         if move_num<20:
-            time.sleep(1)#decision time
+            time.sleep(0.5)#decision time
         move_status = sever_talker(json.dumps({
             "type":"move",
             "id":str(player_id),
@@ -76,14 +79,14 @@ def start_play(id):
     response = requests.post('http://localhost:8001/games/'+game_id+'/tokens/'+str(response.json()['id']))
     assert response.status_code == 200
     token = response.json()['token']
-    print(response.json()['status'])
+    if full_print:print(response.json()['status'])
     ws = websocket.WebSocket()
     ws.connect('ws://localhost:8001/ws/' + game_id, header={'Authorization': token}),
     sever_talker=lambda message:[
-            print(message),
+            full_print and print(message),
             ws.send(message),
             ws.recv(),
-            print("END",message)][-2]
+            full_print and print("END",message)][-2]
     play(id,sever_talker)
 
 
